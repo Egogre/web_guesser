66b8ce5b6a664e9542016aef6e2ad546587b3f48
George Hudson <georgehudson78@gmail.com>
2015-08-17 16:32:58 -0600

    background colors added

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,17 +1,44 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = rand(100)
+SECRET_NUMBER = rand(100)
 
 get '/' do
-  guess = params["guess"].to_i
-  message = "the SECRET NUMBER! You got it right!" if guess == secret_number
-  message = "only a little too high!" if (1..5) === (guess - secret_number)
-  message = "too high!" if (6..15) === (guess - secret_number)
-  message = "WAY TOO HIGH!" if (guess - secret_number) > 15
-  message = "only a bit too low!" if (1..5) === (secret_number - guess)
-  message = "too low!" if (6..15) === (secret_number - guess)
-  message = "WAY TOO LOW!" if (secret_number - guess) > 15
+  guess = params["guess"]
+  message = check_guess(guess)
+  bgcolor = check_color(guess)
 
-  erb :index, :locals => {:secret_number => secret_number, :guess => guess, :message => message}
+  erb :index, :locals => {
+                          :secret_number => SECRET_NUMBER,
+                          :guess => guess,
+                          :message => message,
+                          :bgcolor => bgcolor
+                         }
+end
+
+def check_guess(number)
+  guess = number.to_i
+  message = "#{guess} is the SECRET NUMBER! You got it right!" if guess == SECRET_NUMBER
+  message = "#{guess} is only a little too high!" if (1..5) === (guess - SECRET_NUMBER)
+  message = "#{guess} is too high!" if (6..15) === (guess - SECRET_NUMBER)
+  message = "#{guess} is WAY TOO HIGH!" if (guess - SECRET_NUMBER) > 15
+  message = "#{guess} is only a bit too low!" if (1..5) === (SECRET_NUMBER - guess)
+  message = "#{guess} is too low!" if (6..15) === (SECRET_NUMBER - guess)
+  message = "#{guess} is WAY TOO LOW!" if (SECRET_NUMBER - guess) > 15
+  message = "#{guess} is not between 0 and 100." if guess < 0
+  message = "#{guess} is not between 0 and 100." if guess > 100
+  message = "What could it be?" if number == nil
+  message
+end
+
+def check_color(number)
+  guess = number.to_i
+  difference = (guess - SECRET_NUMBER).abs
+  color = :lightblue if guess == SECRET_NUMBER
+  color = :lightgreen if (1..5) === difference
+  color = :lightyellow if (6..15) === difference
+  color = :pink if difference > 15
+  color = :red if (guess > 100 || guess < 0)
+  color = :white if number == nil
+  color
 end
